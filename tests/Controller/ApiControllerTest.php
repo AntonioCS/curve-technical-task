9afb5fca91a13223159aff1ab6e0d4f373a18e70
AntonioCS <antoniocs@gmail.com>
2020-10-09 17:03:52 +0100

    Added one more testf

--- tests/Controller/ApiControllerTest.php
+++ tests/Controller/ApiControllerTest.php
@@ -15,4 +15,16 @@ class ApiControllerTest extends WebTestCase
 
         self::assertEquals(200, $client->getResponse()->getStatusCode());
     }
+
+    public function testIndexHasContent() : void
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/api/rate');
+        $content = $client->getResponse()->getContent();
+        $data = \json_decode($content, true, 512, JSON_THROW_ON_ERROR);
+
+        self::assertArrayHasKey('Currency-Values', $data);
+        self::assertArrayHasKey('Exchange', $data);
+    }
 }
